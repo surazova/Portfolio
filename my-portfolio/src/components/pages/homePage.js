40999361111bfda74dff76eeb685835b0834eb7b
surazova <sitoraurazova@gmail.com>
2019-03-23 13:28:32 -0400

    Edit

---
+++ my-portfolio/src/components/pages/homePage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+  render() {
+    return <div className="container-fluid">Home Page Content Here</div>;
+  }
+}
+export default Homepage;
