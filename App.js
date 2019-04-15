744e28ae14a7367571d8df6908923ba587c9307f
surazova <sitoraurazova@gmail.com>
2019-04-15 10:34:26 -0400

    edit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+// components
+import Header from "./my-portfolio/src/components/headerComponent/header";
+
+import Footer from "/my-portfolio/src/components/footerComponent/footer";
+
+import Homepage from "/my-portfolio/src/components/pages/homePage";
+
+// includes
+import "./Assets/css/default.min.css";
+
+class App extends Component {
+  render() {
+    return <div className="App" 
+    
+    <Header />
+
+<Homepage />
+
+<Footer />
+
+    />;
+  }
+}
+
+export default App;
